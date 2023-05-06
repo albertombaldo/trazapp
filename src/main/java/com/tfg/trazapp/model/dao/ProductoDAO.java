0748b6756b51ed24ade93c626c155ee3a9866c33
alberto <albertombaldo@gmail.com>
2023-05-06 20:41:51 +0200

    Añadido metodo para buscar un producto por nombre completo

--- src/main/java/com/tfg/trazapp/model/dao/ProductoDAO.java
+++ src/main/java/com/tfg/trazapp/model/dao/ProductoDAO.java
@@ -98,6 +98,45 @@ public class ProductoDAO {
     }
 
     //////////////////////////////////////////////////////////////////////////////////////////
+    public JSONArray getProductoPorNombre(String nombre){
+        JSONArray jarray = null;
+        try {
+            URL url = new URL("http://localhost:8080/trazapp/producto/nombre/" + nombre);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            //Comprobar que la peticion ha sido correcta (codigo 200)
+            int responseCode = conn.getResponseCode();
+            if(responseCode != 200) {
+                throw new RuntimeException("Ocurrió un error " + responseCode);
+            }else {
+                //Abrir un Scanner que lea el flujo de datos de la URL e imprimirlo
+
+                StringBuilder info = new StringBuilder();
+
+                //Abrimos el flujo de datos de la URL dentro del Scanner
+                Scanner sc = new Scanner(url.openStream());
+                while(sc.hasNext()) {
+                    info.append(sc.nextLine());
+                }
+                sc.close();
+                String consulta = "[" + info.toString() +"]";
+                jarray = new JSONArray(consulta);
+                //JSONObject jobjeto = jarray.getJSONObject(0);
+            }
+        } catch (MalformedURLException e) {
+            System.err.println("URL incorrecta");
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            System.err.println("Protocolo incorrecto");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return jarray;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////
     public JSONArray getProductosPorTipo(String tipo){
         JSONArray jarray = null;
         try {
