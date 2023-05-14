75287d0e45a93448989d79a65aea739a337b8b2b
alberto <albertombaldo@gmail.com>
2023-05-14 19:25:15 +0200

    Creado ProductoFinalDAO

---
+++ src/main/java/com/tfg/trazapp/model/dao/ProductoFinalDAO.java
@@ -0,0 +1,52 @@
+package com.tfg.trazapp.model.dao;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class ProductoFinalDAO {
+    public JSONArray getAllProductos(){
+        JSONArray jarray = null;
+        try {
+            URL url = new URL("http://localhost:8080/trazapp/productofinal/");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            //Comprobar que la peticion ha sido correcta (codigo 200)
+            int responseCode = conn.getResponseCode();
+            if(responseCode != 200) {
+                throw new RuntimeException("Ocurrió un error " + responseCode);
+            }else {
+                //Abrir un Scanner que lea el flujo de datos de la URL e imprimirlo
+                StringBuilder info = new StringBuilder();
+                //Abrimos el flujo de datos de la URL dentro del Scanner
+                Scanner sc = new Scanner(url.openStream());
+                while(sc.hasNext()) {
+                    info.append(sc.nextLine());
+                }
+                sc.close();
+                //String consulta = "[" + info.toString() +"]";
+                jarray = new JSONArray(info.toString());
+                JSONObject jobjeto = jarray.getJSONObject(0);
+                System.out.println(jobjeto.get("id_producto"));
+                System.out.println(info);
+            }
+        } catch (MalformedURLException e) {
+            System.err.println("URL incorrecta");
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            System.err.println("Protocolo incorrecto");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return jarray;
+    }
+}
